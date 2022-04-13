42196a4e4e90d1c5361a279ee918717f1c3d5e7a
Hailey <pacboy2004@gmail.com>
2022-04-14 08:27:50 +1000

    commit in case i forget

--- packparse.lua
+++ packparse.lua
@@ -15,10 +15,7 @@ local packparse = {
     -- it then returns a processed string with the words replaced
     -- t needs to be a string and include at least 1 replaceable word
     -- w needs to be a table of words that include a nouns, adjectives, and verbs table
-    -- if the replaeable word is a verb ending with an e that's immediately followed by "ing", it will be replaced with the verb in the past tense
-    -- words surrounded by curly braces are special and are affected by certain conditions
-    -- example: {a/n} will either be "a" or "an" depending on if the next letter is a vowel or not
-    parseFlow = function(t, w)
+    parseWords = function(t, w)
         local r = t
         local n = w.nouns
         local a = w.adjectives
@@ -32,11 +29,42 @@ local packparse = {
         r = string.gsub(r, "%[noun%]", n[rn])
         r = string.gsub(r, "%[adjective%]", a[ra])
         r = string.gsub(r, "%[verb%]", v[rv])
-        r = string.gsub(r, "%{a/n%}", string.sub(n[rn], 1, 1) == "a" and "an" or "a")
         return r
-    end
+    end,
 
-    -- this is like my 10th time rewriting this function and for some reason this is the one that works so we're rolling with it
+    -- parseSpecial takes a string (t) as input
+    -- it then returns a string with the special tags replaced
+    -- t needs to be a string and include at least 1 replaceable word
+    -- special tags:
+    -- {a/n} - is replaced with "a" or "an" depending on the next word
+    parseSpecial = function(t)
+        local r = t
+        -- {a/n} - if the next word starts with a vowel, replace with "an"
+        --       - if the next word starts with a consonant, replace with "a"
+        -- locate the special tag
+        local s = string.find(r, "%{a/n%}")
+        -- keep track of the position of the special tag
+        local s_pos = s
+        -- look at the next letter after the special tag
+        local s_next = string.sub(r, s + 1, s + 1)
+        -- if the next letter is a, e, i, o,
+        return r 
+    end
+    
+
+    -- fixGrammar takes a string (t) as input
+    -- it then returns a processed string with corrected grammar
+    -- t needs to be a string
+    -- the function correct most common grammar errors that will most likely appear in a parsed flow
+    -- errors it covers:
+    --  - verb tenses: fixes the spelling of verbs in past or present tense
+    --      - "-eing" -> "-ing"
+    --      - "makeed" -> "made"
+    --      - "-ming" or "-ning" -> "-mming" or "-nning"
+    --      - "-med" or "ned" -> "-am"
+    --      - and so on
+    --  - more will be added as I find them
+
     
     
 
